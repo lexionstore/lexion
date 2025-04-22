c2fe1fb0f2024e6bb6857424ee5749457e8027ba
lexionstore <zxas4798@naver.com>
2025-04-22 20:04:32 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import LexionHome from "./App";
+import HomePage from "./HomePage"; // 수정된 컴포넌트로 연결
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <LexionHome />
+    <HomePage />
   </React.StrictMode>
 );
